028818cf1e7048b807da7468a99b879d1d97de90
horakhy <leonidas51000@hotmail.com>
2023-03-04 21:28:41 -0300

    Testing with numba

--- mainAPS.py
+++ mainAPS.py
@@ -1,82 +1,50 @@
 import numpy as np
 from numba import jit
+from memory_profiler import profile
+
+# @jit(cache=True)
+def get_f(f, alpha, p):
+    return f + alpha * p
+
+# @jit(cache=True)
+def get_r(r, alpha, w):
+    return r - alpha * w
+
+# @jit(cache=True)
+def get_beta(z, z_prev):
+    return np.linalg.norm(z)**2 / np.linalg.norm(z_prev)**2
 
 @jit(cache=True)
-def get_alpha(A, r, p):
-    """
-    Get the alpha value for the Conjugate Gradient Method.
+def get_p(z, beta, p):
+    return z + beta * p
 
-    Parameters:
-    A (ndarray): Coefficient matrix of the linear system.
-    r (ndarray): Residual vector.
-    p (ndarray): Search direction vector.
-
-    Returns:
-    alpha (float): Alpha value.
-    """
-    alpha = np.dot(r, r) / np.dot(p, A.dot(p))
-
-    # Handle invalid alpha value
-    if not np.isfinite(alpha):
-        alpha = 0
-
-    return alpha
-
-@jit(cache=True)
-def get_beta(rs_new, rs_old):
-    return rs_new / rs_old
-
-@jit(cache=True) 
-def support_operation(r, beta, p):
-    return r + beta * p
-
-@jit(cache=True)
-def cg_normal_error(A, b, x0, max_iter=1000, tol=1e-4):
-    """
-    Conjugate Gradient Method Normal Error algorithm to solve linear system Ax = b.
-
-    Parameters:
-    A (ndarray): Coefficient matrix of the linear system.
-    b (ndarray): Right-hand side vector of the linear system.
-    x0 (ndarray): Initial guess for the solution vector.
-    max_iter (int): Maximum number of iterations.
-    tol (float): Tolerance level for the residual norm.
-
-    Returns:
-    x (ndarray): Solution vector.
-    """
-    x = np.array(x0)
-    r = b - A.dot(x)
-    p = r
-    # residual = tol
-    
-    ap_array = np.zeros_like(b)
-    rs_old = np.dot(r, r)
+@profile
+# @jit(forceobj=True, cache=True)
+def conjugate_gradient_normal_residual(H, g, max_iter=1000, tol=1e-4):
+    n = H.shape[0]
+    f = np.zeros(n)
+    r = g
+    z = H.T.dot(r)
+    p = z
 
     for i in range(max_iter):
-        alpha = get_alpha(A, r, p)
+        w = H.dot(p)
+        alpha = np.linalg.norm(z)**2 / np.linalg.norm(w)**2
 
-        x += alpha * p
-        r -= alpha * ap_array
-        rs_new = np.dot(r, r)
+        f = get_f(f, alpha, p)
+        r = get_r(r, alpha, w)
+        
+        z_prev = z
+        z = H.T.dot(r)
 
-        # Handle zero residual case
-        if rs_new == 0:
+        beta = get_beta(z, z_prev)
+        
+        p = get_p(z, beta, p)
+
+        if(i % 100 == 1):
+            print("Iteration: ", i, "Residual norm: ", np.linalg.norm(r))
+
+        if np.linalg.norm(r) < tol:
             break
 
-        if np.sqrt(rs_new) < tol:
-            break
-
-        beta = get_beta(rs_new, rs_old)
-
-        # Handle invalid beta value
-        if not np.isfinite(beta):
-            beta = 0
-
-        # if(i % 100 == 1):
-        #     print("Iteration: ", i, "Residual norm: ", np.sqrt(rs_new))    
-        p = support_operation(r, beta, p)
-
-        rs_old = rs_new
-
-    return x
+    return f
--- test.py
+++ test.py
@@ -1,34 +1,28 @@
 import numpy as np
-import time
-from memory_profiler import memory_usage
-from mainAPS import cg_normal_error
+from scipy.sparse import random
+from numpy.linalg import norm
+from time import time
+from mainAPS import conjugate_gradient_normal_residual
 
-# Define a larger linear system
-n = 5000
-A = np.random.rand(n, n)
-b = np.random.rand(n)
 
-# Define the true solution
-x_true = np.linalg.solve(A, b)
+def test_conjugate_gradient_normal_residual():
+    # generate a random sparse Hessian matrix and gradient vector
+    n = 5000
+    H = random(n, n, density=0.1, format='csr')
+    g = np.random.randn(n)
 
-# Measure the memory consumption
-# mem_usage = memory_usage((cg_normal_error, (A, b, np.zeros(n))))
+    # compute the true solution using numpy's solve function
+    x_true = np.linalg.solve(H.toarray(), -g)
 
-# Measure the time taken
-try:
-    t_start = time.perf_counter()
-    x = cg_normal_error(A, b, np.zeros(n))
-    t_end = time.perf_counter()
-    time_taken = t_end - t_start
-except Exception as e:
-    print(f"An error occurred: {e}")
-    time_taken = None
+    # time the execution of the conjugate_gradient_normal_residual function
+    t0 = time()
+    x = conjugate_gradient_normal_residual(H, g)
+    t1 = time()
 
-# Check the solution and residual norm
-# if time_taken is not None:
-#     assert np.allclose(x, x_true, rtol=1e-6, atol=1e-6)
-#     assert np.linalg.norm(b - A.dot(x)) < 1e-6
+    # check that the solution is close to the true solution
+    # assert norm(x - x_true) / norm(x_true) < 1e-4
 
-# Print the results
-print(f"Time taken: {time_taken:.3f} seconds")
-# print(f"Memory usage: {max(mem_usage)} MiB")
+    # print the execution time and memory usage
+    print(f"Execution time: {t1-t0:.4f} seconds")
+
+test_conjugate_gradient_normal_residual()
